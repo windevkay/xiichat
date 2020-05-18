de64351a6772b1a17831265bb930a0b6d53340d3
Administrator <administrator@Kolas-MBP.hitronhub.home>
2020-05-18 13:42:40 -0600

    with firebase setup

--- lib/screens/chat.screen.dart
+++ lib/screens/chat.screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (ctx, index) => Container(
-        padding: EdgeInsets.all(8),
-        child: Text('This works'),
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('This works'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/ruq6QYnL5c52oYSXASCe/messages')
+              .snapshots() //allows us to listen to a stream i.e. realtime added data
+              .listen((data) {
+            print(data);
+          });
+        },
       ),
     );
   }
